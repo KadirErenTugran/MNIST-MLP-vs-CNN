db1be64762c10347ef3c9323cc10138f7bfbac57
Kadir Eren Tuğran <kadirerentugran@hotmail.com>
2026-04-02 16:09:31 +0300

    docs: verify model architecture output in terminal

--- 01_MLP_Model/mlp_mnist.py
+++ 01_MLP_Model/mlp_mnist.py
@@ -15,3 +15,33 @@ test_loader = DataLoader(test_set, batch_size=64, shuffle=False)
 
 print(f"Train set size : {len(train_set)} image")
 print(f"Test  set size : {len(test_set)} image ")
+
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SimpleMLP(nn.Module):
+    def __init__(self):
+        super(SimpleMLP,self).__init__()
+        self.fc1 = nn.Linear(784,128)
+
+        self.fc2 = nn.Linear(128,64)
+
+        self.fc3= nn.Linear(64,10)
+    
+    def forward(self,x):
+        x=x.view(-1,28*28)
+
+        x=F.relu(self.fc1(x))
+
+        x=F.relu(self.fc2(x))
+        
+        x=F.relu(self.fc3(x))
+        return x
+    
+device=torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+model= SimpleMLP().to(device)
+
+print(model)
+        
