c1e336b61a7f8ff4fca0a95dcee8d827ff5b87f7
Kadir Eren Tuğran <kadirerentugran@hotmail.com>
2026-04-02 15:47:00 +0300

    feat:download and prepare MNIST data with 64 batch size

---
+++ 01_MLP_Model/mlp_mnist.py
@@ -0,0 +1,17 @@
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+train_set = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+test_set = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+
+train_loader = DataLoader(train_set, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_set, batch_size=64, shuffle=False)
+
+print(f"Train set size : {len(train_set)} image")
+print(f"Test  set size : {len(test_set)} image ")
