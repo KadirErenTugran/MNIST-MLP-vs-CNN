eed02fbbc4a3e80514c94841c712737d20d4bb17
Kadir Eren Tuğran <kadirerentugran@hotmail.com>
2026-04-07 12:07:59 +0300

    feat: add training loop and loss visualization

--- 01_MLP_Model/mlp_mnist.py
+++ 01_MLP_Model/mlp_mnist.py
@@ -1,6 +1,7 @@
 import torch
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from torchvision import datasets,transforms
+import torch.optim as optim
+from  torch.utils.data import DataLoader
 
 transform = transforms.Compose([
     transforms.ToTensor(),
@@ -14,34 +15,61 @@ train_loader = DataLoader(train_set, batch_size=64, shuffle=True)
 test_loader = DataLoader(test_set, batch_size=64, shuffle=False)
 
 print(f"Train set size : {len(train_set)} image")
-print(f"Test  set size : {len(test_set)} image ")
+print(f"Test set size : {len(test_set)} image ")
 
 
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.nn.functional as F 
 
 
 class SimpleMLP(nn.Module):
-    def __init__(self):
+    def __init__(self): 
         super(SimpleMLP,self).__init__()
-        self.fc1 = nn.Linear(784,128)
+        self.fc1 = nn.Linear(784,128)  
 
         self.fc2 = nn.Linear(128,64)
 
         self.fc3= nn.Linear(64,10)
     
     def forward(self,x):
-        x=x.view(-1,28*28)
+        x=x.view(-1,28*28) 
 
         x=F.relu(self.fc1(x))
 
         x=F.relu(self.fc2(x))
         
-        x=F.relu(self.fc3(x))
+        x=self.fc3(x)
         return x
-    
+
+
 device=torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 model= SimpleMLP().to(device)
+print(device,"running...")
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(),lr=0.001)  #lr=> Learning Rate
+
+epochs = 5
+train_losses = []
+
+print("training is starting...")
+
+for epochs in range(epochs):
+    running_loss=0.0
+    for images,labels in train_loader:
+        images,labels = images.to(device),labels.to(device)
+        optimizer.zero_grad()
+        outputs=model(images)
+        loss=criterion(outputs,labels)
+        loss.backward()
+        optimizer.step()
+        running_loss +=loss.item()
+    
+    avg_loss=running_loss/len(train_loader)
+    train_losses.append(avg_loss)
+    print(f"epochs => {epochs} done  MSE=>{avg_loss:.4f}")
+
+
 
 print(model)
         
