4d976377fcdac9558139b5fa28332220528c80d6
Kadir Eren Tuğran <kadirerentugran@hotmail.com>
2026-04-07 12:25:06 +0300

    feat: add training and  evaluation loops with %96.42 accuracy

--- 01_MLP_Model/mlp_mnist.py
+++ 01_MLP_Model/mlp_mnist.py
@@ -69,7 +69,23 @@ for epochs in range(epochs):
     train_losses.append(avg_loss)
     print(f"epochs => {epochs} done  MSE=>{avg_loss:.4f}")
 
+correct = 0
+total = 0
 
+model.eval()
+
+with torch.no_grad():
+    for images,labels in test_loader:
+        images,labels = images.to(device), labels.to(device)
+
+        outputs=model(images)
+
+        _,predicted = torch.max(outputs.data, 1)
+        total+= labels.size(0)
+        correct+=(predicted==labels).sum().item()
+accuracy=100 * correct/total
+
+print(f"Model accuracy on 10,000 test images: %{accuracy:.2f}") # %96.42 accuracy
 
 print(model)
         
